41121daea328394164c29e74acfed12dad8fc2ad
Wansel Lemos <>
2025-09-30 05:42:03 -0400

    Adiciona safelist de classes no arquivo de configuração do Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
     "./dev/**/*.{ts,tsx,js,jsx,html}",
     "./src/**/*.{ts,tsx,js,jsx,html}",
   ],
+  // SAFE LIST para classes que são usadas via JSON
+  safelist: [
+    'text-slate-400',
+    'text-emerald-500',
+    'font-bold',
+    'text-red-500',
+    'text-blue-500',
+    'text-amber-500', // Adicionei esta, caso a use no futuro
+    'text-sky-500',   // Adicionei esta, caso a use no futuro
+    'text-slate-300',
+  ],
   theme: {
     extend: {},
   },
