1efc67f56a3575d067c6ca86a4893ce096e9ab74
Wansel Lemos <>
2025-09-30 07:05:14 -0400

    Adiciona suporte para basePath na função drawInteractionChart e ajusta URLs de ícones no arquivo de dados

--- dev/acessosInteracoes.ts
+++ dev/acessosInteracoes.ts
@@ -4,5 +4,13 @@ import jsonData from './data/access-interactions-data.json';
 // Parseia os dados para converter strings de data em objetos Date
 const data = parseInteractionData(jsonData);
 
+// --- Exclusivo GITHUB pages
+// Define o nome do seu repositório no GitHub
+const NOME_DO_REPOSITORIO = 'dataviz-ppgi';
+// Verifica se está rodando no GitHub Pages para definir o caminho base
+const isGhPages = window.location.hostname.includes('github.io');
+const basePath = isGhPages ? `/${NOME_DO_REPOSITORIO}/` : '/';
+// ---
+
 // Desenha o gráfico
-drawInteractionChart('#interaction-chart', data);
+drawInteractionChart('#interaction-chart', data, { basePath: basePath });
