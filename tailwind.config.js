04c061f3fd3d1b033f2ceaa4e1f4519dc7332ef7
Wansel Lemos <>
2025-09-30 05:49:03 -0400

    Adiciona './dev/*.html' em content para o TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     './dev/index.html', // página de desenvolvimento/teste
+    './dev/*.html', // página de desenvolvimento/teste
     "./dev/**/*.{ts,tsx,js,jsx,html}",
     "./src/**/*.{ts,tsx,js,jsx,html}",
   ],
